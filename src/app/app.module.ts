c5ef38cf85eef8ff0829ad7e60ee49de71e91051
khaledawaza <1khaledawaza@gmail.com>
2023-03-29 03:19:39 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
