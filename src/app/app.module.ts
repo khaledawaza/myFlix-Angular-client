8a2002ab6cc4defb72a8c5db4c40822617eaf8da
khaledawaza <1khaledawaza@gmail.com>
2023-03-30 00:04:57 +0200

    create a director folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,10 +14,27 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { GenreDetailsComponent } from './genre-details/genre-details.component';
+import { DirectorDetailsComponent } from './director-details/director-details.component';
+import { MovieSynopsisComponent } from './movie-synopsis/movie-synopsis.component';
+import { FavoriteMovieCardComponent } from './favorite-movie-card/favorite-movie-card.component';
+
+// Routes definition
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent }, // welcome route, WelcomePageComponent - act as a welcome page
+  { path: 'movies', component: MovieCardComponent }, // movies route definition, points to MovieCardComponent
+  { path: 'profile', component: UserProfileComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' }, // welcome route is also resolution for empty route
+];
 
 @NgModule({
   declarations: [
@@ -22,22 +42,31 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     UserRegistrationFormComponent,
     UserLoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    NavBarComponent,
+    UserProfileComponent,
+    GenreDetailsComponent,
+    DirectorDetailsComponent,
+    MovieSynopsisComponent,
+    FavoriteMovieCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule, 
-    MatCardModule, 
-    MatFormFieldModule, 
-    MatDialogModule, 
-    MatSnackBarModule,
     FormsModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
